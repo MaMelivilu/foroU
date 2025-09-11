46c411907c4c114ae0ec9fe47d936757b09abc94
MaMelivilu <ma.melivilu@duocuc.cl>
2025-09-10 21:31:14 -0300

    primera interfaz

--- forou/src/app/page.tsx
+++ forou/src/app/page.tsx
@@ -2,14 +2,48 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    
     <div>
       <main>
-        
-        
+        <header>
+          {/* Titulo + Logo */}
+          <img src="/Logo_ForoU.png" className="w-19" />
+          <h1>
+            
+            <span className="Foro">Foro</span>
+            <span className="U">U</span>
+          </h1>
+          
+          
 
+
+          {/* Barra de busqueda */}
+          <div className="ml-auto flex">
+            <input 
+              type="text" 
+              placeholder="Que estas buscando?..." 
+              className="px-4 py-2 border border-gray-300 rounded-l-md focus:outline-none w-120" 
+            />
+            <button className="px-4 py-2 bg-blue-500 text-white rounded-r-md border border-gray-300">
+              Buscar
+            </button>
+          </div>
+
+          <div className="ml-auto flex">
+            <button className="px-8 py bg-blue-500 text-white">
+              Login
+            </button>
+            <button className="px-5 py bg-blue-700 text-white">
+              Register
+            </button>
+
+          </div>
+          
+
+        </header>
         
       </main>
-      
     </div>
+    
   );
 }
